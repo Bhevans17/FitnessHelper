9c1268e930ea6bc1ea1df1aefc24c3715d8fa1b2
Brenden <bhevans17@gmail.com>
2020-01-09 13:41:53 -0800

    Added php

---
+++ includes/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="style.css" type="text/css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/aos@2.3.0/dist/aos.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Fitness Helper</title>
+</head>
+
+<body>
+    <!-- Navigation Bar -->
+    <nav>
+        <a class="logo" href="#">FITNESS<span class="logo-span">HELPER</span></a>
+
+        <a class="hamburger-icon" href="#">
+            <i class="fas fa-bars"></i>
+        </a>
+
+        <div class="nav-links">
+            <ul>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Create Account</a>
+                </li>
+                <li>
+                    <a href="#">Login</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
---
+++ index.php
@@ -0,0 +1,99 @@
+<?php include "./includes/header.php"; ?>
+<!-- Main -->
+<main>
+    <div data-aos="fade-up" data-aos-anchor-placement="top-center" class="calculator-wrapper">
+        <h1>Macro Calculator</h1>
+        <img src="./img/undraw_fitness_tracker_3033.svg" alt="">
+        <div class="macro-calculator-results">
+            <h3>Calories: <span>1234</span></h3>
+            <h3>Protein: <span>123</span></h3>
+            <h3>Fats: <span>123</span></h3>
+            <h3>Carbs: <span>1234</span></h3>
+
+            <p>Save these results <a href="#">Create an Account</a></p>
+        </div>
+
+        <form class="macro-calculator" action="#">
+            <div class="row">
+                <div class="col">
+                    <div class="form-group">
+                        <label for="age">Age:</label>
+                        <input class="form-control" id="age" name="age" type="text" placeholder="Age">
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="weight">Weight:</label>
+                        <input class="form-control" id="weight" name="weight" type="text" placeholder="Lbs">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+
+                    <div class="form-group">
+                        <label for="gender">Gender:</label>
+                        <select class="form-control" id="gender" name="gender">
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="height">Height:</label>
+                        <select class="form-control" id="height" name="height">
+                            <option value="#">4ft</option>
+                            <option value="#">4ft 1in</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="form-group">
+                        <label for="activity">Activity:</label>
+                        <select class="form-control" id="activity" name="activity">
+                            <option value="sendentary">Sedentary</option>
+                            <option value="light">Light</option>
+                            <option value="moderate">Moderate</option>
+                            <option value="heavy">Heavy</option>
+                            <option value="ahtlete">Athlete</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="bodyfat">Bodyfat %:</label>
+                        <input class="form-control" id="bodyfat" name="bodyfat" type="text" placeholder="Optional">
+                    </div>
+                </div>
+            </div>
+
+            <button class="form-control" type="submit">Calculate</button>
+        </form>
+    </div>
+    <!-- Here To Help Section -->
+    <div data-aos="fade-left" data-aos-anchor-placement="top-center" class="here-to-help-wrapper">
+        <h2> Here To Help</h2>
+        <img src="./img/undraw_eating_together_tjhx.svg" alt="">
+        <button>Sign Up</button>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non ad eos nostrum fugit unde? Provident
+            tempore
+            impedit, optio dolorum soluta fugit dolorem aspernatur nobis eveniet quos rerum consequatur id
+            similique!
+        </p>
+    </div>
+    <!-- Find Workouts Section -->
+    <div data-aos="fade-right" data-aos-anchor-placement="top-center" class="find-workouts-wrapper">
+        <h2>Find Your Next Workout</h2>
+        <img src="./img/undraw_working_out_6psf.svg" alt="">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non ad eos nostrum fugit unde? Provident
+            tempore
+            impedit, optio dolorum soluta fugit dolorem aspernatur nobis eveniet quos rerum consequatur id
+            similique!
+        </p>
+    </div>
+</main>
+
+<?php include "./includes/footer.php"; ?>
